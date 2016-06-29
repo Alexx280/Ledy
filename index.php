74cb760142baa70badb856d90b673fd3bd58122e
Alexx280 <gs455@mail.ru>
2016-06-29 16:48:14 +0700

    Установка фона, значёк в заголовок окна.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: busygin
+ * Date: 29.06.2016
+ * Time: 15:20
+ */?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Леди Ди</title>
+    <script src="jquery.js"></script>
+    <link rel='stylesheet' type='text/css' href='style.css'/>
+    <link rel="shortcut icon" href="png/Scissors1.png" type="image/png">
+</head>
+<body>
+    <div class="fon">
+        <img src="png/fon02.png"/>
+    </div>
+    <div class="main">
+
+    </div>
+</body>
+<footer>
+    <div>
+
+    </div>
+</footer>
+</html>
