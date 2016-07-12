85b0839116ac0fde327077570869f3d87e978116
Alexx280 <gs455@mail.ru>
2016-07-12 14:32:23 +0700

    Добавлена верхняя панель с кнопками и телефоном.

--- index.php
+++ index.php
@@ -14,10 +14,17 @@
     <link rel="shortcut icon" href="png/Scissors1.png" type="image/png">
 </head>
 <body>
+    <div class="head">
+        <div class="top_button tel">Наш тел.: 89234201508</div>
+        <div class="top_button top_button_1">Главная</div>
+        <div class="top_button top_button_2">Услуги</div>
+        <div class="top_button top_button_3">Адрес</div>
+    </div>
     <div class="fon">
         <img src="png/fon02.png"/>
     </div>
     <div class="main">
+
 
     </div>
 </body>
