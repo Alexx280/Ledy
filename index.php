809ab6e837763f4fd7cc7a49e45161ce3c33c626
Alexx280 <gs455@mail.ru>
2016-07-12 17:44:32 +0700

    Добавил фото и название

--- index.php
+++ index.php
@@ -12,10 +12,11 @@
     <script src="jquery.js"></script>
     <link rel='stylesheet' type='text/css' href='style.css'/>
     <link rel="shortcut icon" href="png/Scissors1.png" type="image/png">
+    <link href='http://fonts.googleapis.com/css?family=Lobster&subset=cyrillic,latin' rel='stylesheet' type='text/css'>
 </head>
 <body>
     <div class="head">
-        <div class="top_button tel">Наш тел.: 89234201508</div>
+        <div class="top_button tel">8-923-420-15-08</div>
         <div class="top_button top_button_1">Главная</div>
         <div class="top_button top_button_2">Услуги</div>
         <div class="top_button top_button_3">Адрес</div>
@@ -24,8 +25,14 @@
         <img src="png/fon02.png"/>
     </div>
     <div class="main">
-
-
+        <img src="pic/foto2.jpg"/>
+        <div class="named">Леди Ди</div>
+        <div class="named salon">Салон красоты</div>
+    </div>
+    <div class="progress">
+        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+            <span class="sr-only">45% Complete</span>
+        </div>
     </div>
 </body>
 <footer>
@@ -33,4 +40,5 @@
 
     </div>
 </footer>
+
 </html>
