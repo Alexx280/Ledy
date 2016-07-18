661e9d553ca178401549508c23593571352d4329
Alexx280 <gs455@mail.ru>
2016-07-18 14:23:58 +0700

    Добавлены кнопки вверху, карта, выпадающее окно с телефоном, переход по странице при нажатии кнопок.

--- index.php
+++ index.php
@@ -16,11 +16,19 @@
 </head>
 <body>
     <div class="head">
-        <div class="top_button tel">8-923-420-15-08</div>
+        <div class="top_button tel">8-923-420-15-08
+            <div class="phone">
+                тел.: 256-296, <br> 8-961-095-6970
+            </div>
+        </div>
+
         <div class="top_button top_button_1">Главная</div>
         <div class="top_button top_button_2">Услуги</div>
         <div class="top_button top_button_3">Адрес</div>
+        <div class="top_button top_button_4">Наши работы</div>
+        <div class="top_button top_button_5">О Нас</div>
     </div>
+
     <div class="fon">
         <img src="png/fon02.png"/>
     </div>
@@ -28,17 +36,54 @@
         <img src="pic/foto2.jpg"/>
         <div class="named">Леди Ди</div>
         <div class="named salon">Салон красоты</div>
-    </div>
-    <div class="progress">
-        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
-            <span class="sr-only">45% Complete</span>
+        <!--<div class="adress">Адрес салона: <br> г. Томск, проспект Фрунзе, 129 <br> тел.: 256-296, 8-961-095-6970 </div>-->
+        <div class="time">Мы работаем: <br> Понедельник - Пятница: 9:00 - 21:00 <br> Суббота - Воскресенье: 10:00 - 20:00</div>
+        <div class="price">
+            <p style="text-align: center">Услуги:</p>
+            <ul>
+                <li>Стрижка женская от 350 руб.</li>
+                <li>Стрижка мужская от 150 руб.</li>
+                <li>Стрижка детская от 150 руб.</li>
+                <li>Химическая завивка волос от 1000 руб.</li>
+                <li>Визаж от 800 руб.</li>
+                <li>Хаэрс тату от 400 руб.</li>
+                <li>Лечение волос от 800 руб</li>
+                <li>Оформление и покраска бровей от 150 руб.</li>
+                <li>Маникюр от 300руб.</li>
+                <li>Педикюр от 1000 руб.</li>
+                <li></li>
+            </ul>
+        </div>
+        <div class="map">
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=DyJCZRn5MeAN_ZM1e1HeJX96G79l31CK&width=615&height=388&lang=ru_RU&sourceType=constructor&scroll=true"></script>
+            <div class="adress">Адрес салона: г. Томск, проспект Фрунзе, 129 <br> тел.: 256-296, 8-961-095-6970 </div>
         </div>
     </div>
+    <div class="footer">
+        &copy; Разработка и поддержка сайта <a href="http://alexeybusygin.ru" style="text-decoration: none; color: #ffffff">Бусыгин Алексей <img src="pic/Contact-16.png"/></a>
+    </div>
+
 </body>
 <footer>
     <div>
 
     </div>
 </footer>
-
+<script>
+    $('.top_button_1').click(function(){
+        $('body,html').animate({scrollTop: 0},800); return false;
+    });
+    $('.glav').click(function(){
+        $('body,html').animate({scrollTop: 0},800); return false;
+    });
+    $('.top_button_3').click(function(){
+        $('body,html').animate({scrollTop: 900},800); return false;
+    });
+    $('.price').click(function(){
+        $('body,html').animate({scrollTop: 8500},1500); return false;
+    });
+    $('.button-close').click(function(){
+        $('.discount').removeClass('vision');
+    });
+</script>
 </html>
