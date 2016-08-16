07d6c6026fc45a07185430b6da7e830658e2c384
Alexx280 <gs455@mail.ru>
2016-08-16 17:31:56 +0700

    Модернизирован внешний вид

--- say.php
+++ say.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: busygin
- * Date: 15.08.2016
- * Time: 14:40
- */
+include_once ('head.tpl');
+?>
+    <div class="say">
+        <p  style="text-align: center">Новый год 2017</p>
+        <br><br>
+        Предоставляем вам скидку 20% с 1 по 8 января если придёте стричься с супругом.
+
+    </div>
+<?php
+include_once ('footer.tpl');
+?>
