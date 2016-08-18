52e41796bc1c55368cee582a5945b2fd62942dbb
Alexx280 <gs455@mail.ru>
2016-08-18 15:18:51 +0700

    Добавлен раздел "отзывы", и подключение к локальной базе

--- say.php
+++ say.php
@@ -1,12 +1,48 @@
 <?php
+
+
 include_once ('head.tpl');
+
+function podkl($config){
+    $pod =new mysqli($config["database"]["host"], $config["database"]["username"], $config["database"]["password"],$config["database"]["name"]);
+    $pod->query("SET NAMES utf8");
+    return $pod;
+}
+
+function connect(){
+    $config = parse_ini_file('config/config.ini', true);
+    $link =podkl($config);
+    return $link;}
+if ($_POST['myname']){
+    $link=connect();
+    $login= "INSERT INTO `say` (`name`, `e-mail`, `date`, `text`) VALUES ('".$_POST['myname']."', '".$_POST['mymail']."', '".date('y-m-d')."', '".$_POST['mytext2']."');";
+    $res = $link->query($login) ;
+}
+else {}
+//$row = $res->fetch_assoc();
+
 ?>
     <div class="say">
-        <p  style="text-align: center">Новый год 2017</p>
-        <br><br>
-        Предоставляем вам скидку 20% с 1 по 8 января если придёте стричься с супругом.
+        <form name="test" method="post" action="#">
+            <p><b>Ваше имя:</b><br>
+                <input type="text" size="40" name="myname">
+            </p>
+            <p><b>Ваш e-mail:</b><br>
+                <input type="text" size="40" name="mymail">
+            </p>
+            <p>Комментарий<Br>
+                <textarea cols="50" rows="5" name="mytext2"></textarea> <br>
+                <input type="submit" value="Отправить">
+                <input type="reset" value="Очистить"></p>
+        </form>
+
 
     </div>
 <?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 include_once ('footer.tpl');
+
 ?>
